03eac0cdd220431d5e17551b9e9b12909af781dd
Spokai3 <90892870+Spokai3@users.noreply.github.com>
2024-05-07 00:07:45 +0700

    Add files via upload
    Fix app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Login from './Pages/Login.jsx';
+import MobileLogin from './Pages/MobileLogin.jsx';
+import MobileOTP from './Pages/MobileOTP.jsx';
+import EmailLogin from './Pages/EmailLogin.jsx';
+import EmailOTP from './Pages/EmailOTP.jsx';
+import WeatherForecast from './Components/WeatherForecast/WeatherForecast.jsx';
+import SidebarMenu from './Components/SidebarMenu/SidebarMenu.jsx';
+import Layout from './Layout.jsx';
+
+ReactDOM.render(<WeatherForecast />, document.getElementById('root'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <BrowserRouter>
+      <Navbar />
+      <WeatherForecast />      
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/MobileLogin' element={<MobileLogin/>}/>
+        <Route path='/MobileOTP' element={<MobileOTP/>}/>                 
+        <Route path='/EmailLogin' element={<EmailLogin/>}/>
+        <Route path='/EmailOTP' element={<EmailOTP/>}/>         
+        
+      </Routes>      
+      </BrowserRouter>
     </div>
   );
 }
